91b4d00d63b95cebf276da5a9319316e54d96b13
CBT <bhavanigmgowda@gmail.com>
2019-10-11 12:11:00 +0530

    navbar

--- FrontEnd/src/components/navBar/SearchNavBar.js
+++ FrontEnd/src/components/navBar/SearchNavBar.js
@@ -67,11 +67,11 @@ class SearchNavabar extends Component {
         return (
             <div>
                   <Navbar style={{height:'60px'}} bg="dark" expand="lg">
-                   <Link className="navbar-brand" to="/homePage" onClick={this.props.clearSearch} style={{color:'white'}}>Task Manager</Link>
+                   <Link className="navbar-brand" to="/homePage" onClick={this.props.clearSearch} style={{color:'white'}}><h3>Task Manager</h3></Link>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <div inline   inline className="row container-fluid ml-5  mr-n5 justify-content-center">
-                        <div class="col-sm-3- ">
+                        <div class="col-sm-3- offset-2 ">
                             <FormControl type="text" name="search"  
                                 onChange={(event) => { this.setState({ search: event.target.value }) }}
                                    value={this.state.search} onKeyDown={(event) => { this.searchByEnter(event) }}  placeholder="Search" className="w-55" />
@@ -88,13 +88,23 @@ class SearchNavabar extends Component {
                          </Button></div>
                             </Link>
                             </Nav>
-                            <Nav className="nav-link">                            
-                            <NavDropdown style={{color:'white'}}   title={<Image style={{width:'29px'}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3ub9pAdXyp3SunhYlBjGcqu82RL20pRSumjUnJ3e2tRiF_bS4" roundedCircle />}  alignRight
-  id="dropdown-menu-align-right">
-                                <NavLink className="nav-link" onClick={this.props.clearSearch} onClick={this.logout.bind(this)}>Logout</NavLink>
-                                <NavLink className="nav-link" onClick={this.props.clearSearch} to="/myprofile">MyProfile</NavLink>
-                            </NavDropdown>
-                        </Nav>
+                               
+
+
+<div class="dropdown-nav">
+    <button  style={{color:'white'}}  class="dropbtn-nav"><Image style={{width:'29px'}} 
+    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3ub9pAdXyp3SunhYlBjGcqu82RL20pRSumjUnJ3e2tRiF_bS4" roundedCircle /> &nbsp;
+        <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content-nav">
+    <NavLink className="nav-link linkbar-nav" onClick={this.props.clearSearch} onClick={this.logout.bind(this)}>Logout</NavLink>
+      <NavLink className="nav-link linkbar-nav" onClick={this.props.clearSearch} to="/myprofile">MyProfile</NavLink>
+                                 </div>
+</div>
+
+
+
+                        
                     </Navbar.Collapse>
 
                 </Navbar>
