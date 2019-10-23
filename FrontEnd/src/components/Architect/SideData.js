2cc0667365d8c249a90c6cd854137e7d1508b55c
CBT <bhavanigmgowda@gmail.com>
2019-10-23 15:51:24 +0530

    src folder

---
+++ FrontEnd/src/components/Architect/SideData.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+
+
+export const Architect = () => {
+
+    return (
+        <div>        
+            <div className="input-group-prepend">
+        <i class="fas fa-users iconTask"> &nbsp; Create Project</i>												
+    </div>
+    <div className="input-group-prepend">
+        <i class="fas fa-tasks iconTask">&nbsp; My Task</i>	
+        </div>	
+        <div className="input-group-prepend">
+        <i class="fas fa-tasks iconTask">&nbsp; Assigned Task</i>	
+        </div>		
+        <div className="input-group-prepend">
+        <i class="fas fa-th-list iconTask" style={{marginTop:"20"}}>&nbsp; Completed Task</i>
+        </div>
+        <div className="input-group-prepend">
+        <i class="fas fa-check-circle iconTask" style={{marginTop:"20"}}>&nbsp; Completed Project</i>
+        </div>	
+        <div className="input-group-prepend">
+        <i class="fas  fa-plus-circle iconTask">&nbsp; Add User</i>
+        </div>	
+        </div>
+	
+     )
+}
+
+export const Lead = () => {
+
+    return (
+        <div>        
+        <div className="input-group-prepend">
+    <i class="fas fa-users iconTask"> &nbsp; Create Project</i>												
+</div>
+<div className="input-group-prepend">
+    <i class="fas fa-tasks iconTask">&nbsp; My Task</i>	
+    </div>	
+    <div className="input-group-prepend">
+    <i class="fas fa-tasks iconTask">&nbsp; Assigned Task</i>	
+    </div>		
+    <div className="input-group-prepend">
+    <i class="fas fa-th-list iconTask" style={{marginTop:"20"}}>&nbsp; Completed Task</i>
+    </div>
+    <div className="input-group-prepend">
+    <i class="fas fa-check-circle iconTask" style={{marginTop:"20"}}>&nbsp; Completed Project</i>
+    </div>	
+ 	
+    </div>
+	
+     )
+}
+
+
+export const Employee = () => {
+
+    return (
+        <div>        
+    
+<div className="input-group-prepend">
+    <i class="fas fa-tasks iconTask">&nbsp; My Task</i>	
+    </div>	
+    <div className="input-group-prepend">
+    <i class="fas fa-tasks iconTask">&nbsp; Assigned Task</i>	
+    </div>		
+    <div className="input-group-prepend">
+    <i class="fas fa-th-list iconTask" style={{marginTop:"20"}}>&nbsp; Completed Task</i>
+    </div>
+    <div className="input-group-prepend">
+    <i class="fas fa-check-circle iconTask" style={{marginTop:"20"}}>&nbsp; Completed Project</i>
+    </div>	
+ 	
+    </div>
+	
+     )
+}
